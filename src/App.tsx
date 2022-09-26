8bd5ee8b8620734dcc76241e0a64a35db09fee47
antonTykhomyrovWix <antont@wix.com>
2022-09-26 15:58:17 +0300

    Extend NavigationContainer with empty Article page

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Home } from "./pages/home";
+import { Article } from "./pages/article";
 
 const Stack = createNativeStackNavigator();
 
 export function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Article" component={Article} />
       </Stack.Navigator>
     </NavigationContainer>
   );
