d51bfa76a9de7944561abaa88f0278ce418f4206
antonTykhomyrovWix <antont@wix.com>
2022-09-26 14:09:31 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Real World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
